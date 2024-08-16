2ae8599a35382b75b5fd185e508577d6c5ac9e1f
Alan Romano <alan.rom25.95@gmail.com>
2024-08-16 11:05:29 -0400

    VAC-17 Remove Demo Ruby on Rails Code

--- frontend/web/db/migrate/20240816115953_add_status_to_articles.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToArticles < ActiveRecord::Migration[7.1]
-  def change
-    add_column :articles, :status, :string
-    change_column_default :articles, :status, from: nil, to: "public"
-  end
-end
--- frontend/web/db/migrate/20240816120020_add_status_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToComments < ActiveRecord::Migration[7.1]
-  def change
-    add_column :comments, :status, :string
-    change_column_default :comments, :status, from: nil, to: "public"
-  end
-end
